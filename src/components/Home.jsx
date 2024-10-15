ca8882c219c75f32b23be0ac5783090133fcff00
M Santosh <sunny962002@gmail.com>
2024-10-15 17:05:43 +0530

    Home Page Half

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,11 +1,18 @@
-import React from 'react'
+import React from "react";
+import HomeBanner from "./Home/HomeBanner";
+import HomeServices from "./Home/HomeServices";
+import HomeIndustires from "./Home/HomeIndustires";
+import HomeUniqueness from "./Home/HomeUniqueness";
 
 function Home() {
   return (
-    <div>
-      
+    <div className="Home">
+      <HomeBanner />
+      <HomeServices />
+      <HomeIndustires />
+      <HomeUniqueness />
     </div>
-  )
+  );
 }
 
-export default Home
+export default Home;
